de6aeae675c196220c94b1749c7b915d4cb308f4
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2026-05-02 21:29:27 +0530

    fix(lint): suppress hydration state lint error in SettingsModal

--- components/layout/SettingsModal.tsx
+++ components/layout/SettingsModal.tsx
@@ -16,6 +16,7 @@ export const SettingsModal = ({ isOpen, onClose }: SettingsModalProps) => {
   const [isClearing, setIsClearing] = useState(false);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
